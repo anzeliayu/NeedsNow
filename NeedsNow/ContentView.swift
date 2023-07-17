3e2b7941962fa9625a0347e4ce990e466a075172
Jenny Choi <jennychoi2005@gmail.com>
2023-07-17 15:32:18 -0700

    hi

--- NeedsNow/ContentView.swift
+++ NeedsNow/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Welcome all!")
+            Text("Jenny")
         }
         .padding()
     }
