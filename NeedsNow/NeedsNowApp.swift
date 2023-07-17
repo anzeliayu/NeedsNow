72910e75f3494d24c265b5e6911c98bcf20b1faa
Anzelia Yu <anzeliayu2028@gmail.com>
2023-07-17 15:16:29 -0700

    Initial Commit

---
+++ NeedsNow/NeedsNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeedsNowApp.swift
+//  NeedsNow
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct NeedsNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
